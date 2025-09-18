bad453f8f4e74e8a4256bd5a5933f41318428128
ViktorNikoriak <nikoriakviktor@gmail.com>
2025-09-18 23:52:48 +0300

    11 comit

--- services/dash/pages/test_map.py
+++ services/dash/pages/test_map.py
@@ -1,20 +1,18 @@
+# pages/test_map.py
 import dash
-from dash import html, dcc, callback
+from dash import html, dcc, callback, Input, Output
 import dash_leaflet as dl
 import json
 import geopandas as gpd
-from dash.dependencies import Input, Output
-from config import gdf_basin
+from registry import get_df
 
-
-print("Loaded test page")  # показує, що файл точно імпортується
+print("Loaded test page")
 
 dash.register_page(__name__, path="/test", name="Dashboard", order=1)
 
-# Читання шару басейну (GeoJSON)
+# Читання шару басейну (через registry)
 try:
-    # basin = gpd.read_file("data/basin_bil_cher_4326.gpkg")
-    basin = gdf_basin
+    basin: gpd.GeoDataFrame = get_df("basin")
     print("Basin loaded! CRS:", basin.crs)
     basin = basin.to_crs("EPSG:4326")
     basin_json = json.loads(basin.to_json())
@@ -45,7 +43,7 @@ layout = html.Div([
         ) if basin_json is not None else html.Div("❌ Basin not loaded!"),
         dl.TileLayer(
             id="dem-tile-test",
-            url="/tc/singleband/dem/fab_dem/{z}/{x}/{y}.png?colormap=viridis&stretch_range=[0,2200]",  # Дефолтний URL
+            url="/tc/singleband/dem/fab_dem/{z}/{x}/{y}.png?colormap=viridis&stretch_range=[0,2200]",
             opacity=0.7
         )
     ], style={'width': '100%', 'height': '700px'}, center=[47.8, 25.03], zoom=10)
@@ -55,15 +53,10 @@ layout = html.Div([
     Output("dem-tile-test", "url"),
     Input("colormap-dropdown-test", "value")
 )
-def update_tile_url(colormap):
+def update_tile_url(colormap: str):
     try:
-        print("Callback called! colormap:", colormap)
         stretch = "[0,2200]"
         url = f"/tc/singleband/dem/fab_dem/{{z}}/{{x}}/{{y}}.png?colormap={colormap}&stretch_range={stretch}"
-        print("Returning url:", url)
         return url
-    except Exception as e:
-        import traceback
-        print("EXCEPTION in update_tile_url:", e)
-        traceback.print_exc()
+    except Exception:
         return dash.no_update
