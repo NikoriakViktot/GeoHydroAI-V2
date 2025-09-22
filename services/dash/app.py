62d2bfa5db87b989fbc1b8f45e8bb4993220ad31
ViktorNikoriak <nikoriakviktor@gmail.com>
2025-09-22 15:20:31 +0300

    57 comit

--- services/dash/app.py
+++ services/dash/app.py
@@ -41,7 +41,7 @@ for mod in (
     "callbacks.sidebar_drawer",
     "callbacks.cdf_callback",
     "callbacks.best_model_callback",
-    "callbacks.map_profile_callback",
+    # "callbacks.map_profile_callback",
     "callbacks.profile_callback",
 ):
     try:
