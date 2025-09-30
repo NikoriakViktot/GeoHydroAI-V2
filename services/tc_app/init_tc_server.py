2a2519c48d247b953bd77f7d37d4fcbfdfc154ed
ViktorNikoriak <nikoriakviktor@gmail.com>
2025-09-30 12:17:11 +0300

    dash  dem diff visual

--- services/tc_app/init_tc_server.py
+++ services/tc_app/init_tc_server.py
@@ -3,6 +3,7 @@ from terracotta import  update_settings
 from terracotta.server import create_app
 from flask import jsonify
 from config import DRIVER_PATH, TC_PORT, TC_HOST
+from flask import Response
 
 update_settings(
     DRIVER_PATH=str(DRIVER_PATH),
@@ -13,3 +14,25 @@ update_settings(
 tile_server = create_app()
 
 tile_server.add_url_rule("/health", "health", lambda: jsonify(status="ok"))
+
+
+@tile_server.route("/apidoc")
+def apidoc():
+    html = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <meta charset="utf-8"/>
+      <title>Terracotta API</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <style> body { margin:0; padding:0; } </style>
+      <!-- CDN ReDoc -->
+      <script src="https://cdn.jsdelivr.net/npm/redoc@next/bundles/redoc.standalone.js"></script>
+    </head>
+    <body>
+      <!-- ВАЖЛИВО: відносний шлях, щоб працювало під /tc/ -->
+      <redoc spec-url="./swagger.json"></redoc>
+    </body>
+    </html>
+    """
+    return Response(html, mimetype="text/html")
